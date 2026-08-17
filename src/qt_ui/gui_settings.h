d843c497da970f8272a4e968cedc0f1f30f8350b
georgemoralis <giorgosmrls@gmail.com>
2026-08-17 04:51:12 +0300

    part4

--- src/qt_ui/gui_settings.h
+++ src/qt_ui/gui_settings.h
@@ -88,7 +88,6 @@ const GUISave compatibility_json_url =
             "download/compatibility_data.json");
 const GUISave compatibility_issues_url =
     GUISave(compatibility, "issues_url", "https://github.com/shadps5/shadps5-game-compatibility/");
-const GUISave compatibility_bypass_loganalyzer = GUISave(general, "bypass_loganalyzer", false);
 
 // main window settings
 const GUISave main_window_gamelist = GUISave(main_window, "gamelistVisible", true);
