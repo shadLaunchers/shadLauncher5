06f353c0b125d2ab2a7af89f4a9ffdbd3f678b84
georgemoralis <giorgosmrls@gmail.com>
2026-08-17 13:51:23 +0300

    log settings

--- src/qt_ui/log_presets_dialog.cpp
+++ src/qt_ui/log_presets_dialog.cpp
@@ -1,4 +1,6 @@
 // SPDX-FileCopyrightText: Copyright 2025-2026 shadPS4 Emulator Project
+// SPDX-FileCopyrightText: Copyright 2025-2026 shadLauncher4 Project
+// SPDX-FileCopyrightText: Copyright 2026 shadLauncher5 Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "log_presets_dialog.h"
